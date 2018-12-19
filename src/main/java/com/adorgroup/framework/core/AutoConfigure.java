b06038e364c3306b2b3d634dc307d5bc11e36fe2
Sorta <sorta@163.com>
2018-12-19 18:31:59 +0800

    starter

---
+++ src/main/java/com/adorgroup/framework/core/AutoConfigure.java
@@ -0,0 +1,7 @@
+package com.adorgroup.framework.core;
+
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan(basePackages = {"com.adorgroup.framework.core"})
+public class AutoConfigure {
+}
