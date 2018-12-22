b592d2c84d56b78c564bd1c7780bac0afa00ead3
Sorta <sorta@163.com>
2018-12-22 15:20:43 +0800

    starter

--- src/main/java/com/adorgroup/framework/core/interceptor/CheckTokenInterceptor.java
+++ src/main/java/com/adorgroup/framework/core/interceptor/CheckTokenInterceptor.java
@@ -1,12 +1,11 @@
 package com.adorgroup.framework.core.interceptor;
 
-import com.adorgroup.framework.core.security.user.token.EncryptedToken;
 import com.adorgroup.framework.common.exception.error.BaseBusinessModuleException;
 import com.adorgroup.framework.common.exception.error.DefaultError;
 import com.adorgroup.framework.core.security.annotation.IgnoreTokenChecking;
 import com.adorgroup.framework.core.security.user.Token;
+import com.adorgroup.framework.core.security.user.token.EncryptedToken;
 import com.adorgroup.framework.core.security.user.token.TokenUtil;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.util.StringUtils;
@@ -18,30 +17,37 @@ import javax.servlet.http.HttpServletResponse;
 import java.lang.reflect.Method;
 
 @Configuration
-public class  CheckTokenInterceptor extends HandlerInterceptorAdapter {
+public class CheckTokenInterceptor extends HandlerInterceptorAdapter {
 
     @Value("${framework.secrety.secretKey}")
     private String secretKey;
     @Value("${framework.secrety.algorithm:DES}")
     private String algorithm;
+    @Value("${framework.secrety.overtime:-1}")
+    private Long overtime;
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 
         final HandlerMethod handlerMethod = (HandlerMethod) handler;
         final Method method = handlerMethod.getMethod();
-        if(method.getAnnotationsByType(IgnoreTokenChecking.class).length>0){
+        if (method.getAnnotationsByType(IgnoreTokenChecking.class).length > 0) {
             return true;
         }
 
         String token = request.getParameter("token");
-        if(StringUtils.isEmpty(token)){
+        if (StringUtils.isEmpty(token)) {
             throw new BaseBusinessModuleException(DefaultError.TOKEN_NOT_FOUND);
         }
 
         Token t = new EncryptedToken(secretKey, algorithm, token);
+        if (overtime > 0 && System.currentTimeMillis() - t.getCreatedTime() >= overtime) {
+            throw new BaseBusinessModuleException(DefaultError.TOKEN_OVERTIME);
+        }
+
         TokenUtil.setToken(t);
         return true;
+
     }
 
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
